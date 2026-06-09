7832ac002d3945ade9369312fae5ca6b1ae52fac
Shurik <sanya.kucheryavvkh@bk.ru>
2026-06-09 16:08:51 +0300

    add: validation post answer

--- src/college_parser/utils/validation_post_response.py
+++ src/college_parser/utils/validation_post_response.py
@@ -1,4 +1,3 @@
-import asyncio
 from pydantic import ValidationError
 
 from src.college_parser.models.post_response import PostAnswer
@@ -6,37 +5,36 @@ from src.college_parser.services.auth_services import get_post_response
 
 class ValidPostResponse:
     def __init__(self, post_response: dict[str, str | int]) -> None:
-        init_post_response = post_response
-        if not isinstance(init_post_response, dict):
+        if not isinstance(post_response, dict):
             raise ValueError(f"""
             Переданный объект post request некорректен.
             Тип переданного объекта: {type(post_response)}""")
 
-        if init_post_response == {}:
+        if post_response == {}:
             raise ValueError("словарь ответа post запроса пуст")
 
-        self.post_response = init_post_response
+        self.post_response = post_response
+        self.valid_post_answer: PostAnswer | None = None
         self.refresh_token: str | None = None
-        self.valid_post_answer = None
 
-    def _validation_response(self) -> dict[str, str | int]:
+    def _validation_response(self) -> PostAnswer:
         try:
             valid_data: PostAnswer = PostAnswer(**self.post_response)
-            transformation_valid_data = valid_data.model_dump()
-            self.valid_post_answer = transformation_valid_data
-            self.refresh_token = transformation_valid_data.get('refresh_token')
+            self.valid_post_answer = valid_data
+            self.refresh_token = valid_data.refresh_token
             return self.valid_post_answer
         except ValidationError as error:
-            raise ValidationError(f'Возникла ошибка при валидации пост ответ: {error}')
+            raise ValidationError(f'Возникла ошибка при валидации пост ответ: {error}') from error
 
-    def get_valid_data(self) -> str | None:
-        if self._validation_response() is not None:
-            return self.refresh_token
+    def get_valid_refresh_token(self) -> str:
+        self._validation_response()
+        if self.refresh_token is None:
+            raise ValueError("Токен не прошел валидацию")
         else:
-            print("Валидных данных нет")
+            return self.refresh_token
 
 async def get_refresh_token() -> str:
     post_data = await get_post_response()
     valid_post = ValidPostResponse(post_data)
-    result_valid: str | None = valid_post.get_valid_data()
+    result_valid: str = valid_post.get_valid_refresh_token()
     return result_valid
