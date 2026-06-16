623473b87d301f84f22dab74787c0eec75b1044e
Shurik <sanya.kucheryavvyh@bk.ru>
2026-06-16 20:41:36 +0300

    update validtion parsing a data

--- src/college_parser/utils/validation_get_response.py
+++ src/college_parser/utils/validation_get_response.py
@@ -1,26 +1,16 @@
-from typing import List, Dict, Optional
-from pydantic import ValidationError
+from typing import List, Dict
 import asyncio
-from datetime import datetime
+
+from pydantic import ValidationError
+
 
 from src.college_parser.services.parser_service import get_parsing_data
 from src.college_parser.models.lesson import GetResponse
-
 
 class ValidGetResponse:
     """
     Валидатор и форматер дат и расписания
     """
-
-    def __init__(self, parsing_data: List[Dict[str, str | int]]) -> None:
-        if not isinstance(parsing_data, list):
-            raise ValueError("Требуется список с данными")
-        if not parsing_data:
-            raise ValueError("Список с данными пуст")
-
-        self.parsing_data = parsing_data
-        self._valid_schedule: List[Dict] | None = None  # 👈 храним словари
-
     @staticmethod
     def _validation_parsing_data(parsing_data: List[Dict[str, str | int]]) -> List[Dict]:
         """Валидирует данные и возвращает список словарей"""
@@ -34,38 +24,14 @@ class ValidGetResponse:
         return lines
 
     @staticmethod
-    def _formater_date_today() -> str:
-        today = datetime.now()
-        return today.strftime("%Y-%m-%d")
+    def get_valid_data(parsing_data) -> list:
+        return ValidGetResponse._validation_parsing_data(parsing_data)
 
-    @staticmethod
-    def _formater_schedule(valid_schedule: List[Dict]) -> str:
-        """Форматирует расписание для вывода"""
-        lines = ['\n']
-        today = ValidGetResponse._formater_date_today()
-
-        for data in valid_schedule:
-            if data.get('date') == '2026-06-11':
-                lines.append(f"📚Пара: {data.get('subject_name')} |👨‍🏫 Преподаватель: {data.get('teacher_name')}")
-                lines.append(f"🕐Начало {data.get('started_at')} | 🏁 Конец {data.get('finished_at')}")
-                lines.append(f"🏛️Аудитория: {data.get('room_name')}")
-                lines.append("----------------------------------------------------------------------")
-
-        return "\n".join(lines)
-
-    def get_ready_schedule(self) -> str:
-        """Главный метод для получения готового расписания"""
-        valid_data = self._validation_parsing_data(self.parsing_data)
-        # Форматируем и возвращаем
-        return self._formater_schedule(valid_data)
-
-
-async def get_schedule_today() -> str:
-    """Получает расписание на сегодня"""
+async def get_valid_schedule() -> List[Dict[str, str | int]]:
+    """ Получаем валидные данные """
     parsing_data: List[Dict[str, str | int]] = await get_parsing_data()
     try:
-        valid_obj: ValidGetResponse = ValidGetResponse(parsing_data)
-        result = valid_obj.get_ready_schedule()
-        return result
+        result_valid = ValidGetResponse.get_valid_data(parsing_data)
+        return result_valid
     except ValueError as error:
         raise ValueError(f"Некорректно переданный аргумент: {error}") from error
