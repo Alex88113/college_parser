0637fe99102ecd74dfef9f8452a0069a90ca784e
Alex88113 <sanya.kucheryavvkh@bk.ru>
2026-07-28 22:24:06 +0300

    Refactor parsing service for improved error handling

--- src/college_parser/services/parser_service.py
+++ src/college_parser/services/parser_service.py
@@ -29,12 +29,13 @@ class ParsingService:
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 YaBrowser/26.4.0.0 Safari/537.36"
         }
 
-    async def _parsing_schedule(self, token: str) -> Response | None:
+    async def _parsing_schedule(self, token: str) -> Response:
         headers = ParsingService()._request_get_headers(token)
         async with httpx.AsyncClient() as client:
             try:
                 response = await client.get(self._url, headers=headers)
                 response.raise_for_status()
+                return response
             except httpx.HTTPStatusError as error:
                 if error.response.status_code == 401:
                     logger.error(f"Не удалось пройти авторизацию по эндпоинту: {response.url}."
@@ -48,17 +49,11 @@ class ParsingService:
 
     async def get_parsing_data(self, token: str) -> List[Dict[str, str | int]] | None:
         data = await self._parsing_schedule(token)
-        if data:
-            return data.json()
-        return None
+        return data.json()
 
-async def get_parsing_schedule() -> List[Dict[str, str | int]] | None:
+async def get_parsing_schedule() -> List[Dict[str, str | int]]:
     token = await get_tokens()
-    logger.info(f'Token from cache: {token}')
     parsing = ParsingService()
     result_parsing = await parsing.get_parsing_data(token['access_token'])
-    if result_parsing:
-        return result_parsing
-    else:
-        logger.info("Данных нет.")
-        return  None
+    return result_parsing
+    
